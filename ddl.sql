430f9851ad75cc34ae48eccef009e1cc1fd3030f
Hadiqa <hadiqaimrannn0@gmail.com>
2026-07-04 14:28:11 +0300

    added new tables and more defined attributes with better suited data types

--- ddl.sql
+++ ddl.sql
@@ -1,8 +1,8 @@
 CREATE TABLE Bank(
 	bank_id BIGINT GENERATED ALWAYS AS IDENTITY,
 	legal_name VARCHAR(100) NOT NULL UNIQUE,
-	swift_code VARCHAR(11) NOT NULL UNIQUE CHECK(LENGTH(swift_code) =15),
-	routing_no VARCHAR(9) NULL CHECK(LENGTH(routing_no)=9),
+	swift_code SMALLINT NOT NULL UNIQUE CHECK(LENGTH(swift_code) =15),
+	routing_no SMALLINT NULL CHECK(LENGTH(routing_no)=9),
 	country VARCHAR(50) NOT NULL,
 	created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	
@@ -13,12 +13,14 @@ CREATE TABLE Branch(
 	branch_id BIGINT GENERATED ALWAYS AS IDENTITY,
 	name VARCHAR(100) NOT NULL,
 	region VARCHAR(50) NOT NULL,
+	branch_addres VARCHAR(50) NOT NULL,
+	
 PRIMARY KEY (bank_id, branch_id), FOREIGN KEY(bank_id) REFERENCES Bank(bank_id) ON DELETE CASCADE, CONSTRAINT uniq_bank_branch_name UNIQUE(bank_id,name)
 );
 
 CREATE TABLE Role (
     bank_id BIGINT NOT NULL,
-    role_id BIGINT NOT NULL,
+    role_id SMALLINT NOT NULL,
     role_name VARCHAR(50) NOT NULL,
     PRIMARY KEY (bank_id, role_id),
     FOREIGN KEY (bank_id) REFERENCES Bank(bank_id) ON DELETE CASCADE,
@@ -27,9 +29,9 @@ CREATE TABLE Role (
 
 CREATE TABLE Employee(
 bank_id BIGINT NOT NULL,
-employee_id BIGINT GENERATED ALWAYS AS IDENTITY,
-branch_id BIGINT NOT NULL,
-role_id VARCHAR(30) NOT NULL,
+employee_id SMALLINT GENERATED ALWAYS AS IDENTITY,
+branch_id SMALLINT NOT NULL,
+role_id SMALLINT NOT NULL,
 salary DECIMAL(12,2) NOT NULL CHECK (salary >= 0),
 is_active BOOLEAN NOT NULL DEFAULT TRUE,
 PRIMARY KEY(bank_id, employee_id), 
@@ -39,29 +41,29 @@ FOREIGN KEY(bank_id, role_id) REFERENCES role(bank_id, role_id)
 
 CREATE TABLE Party (
     bank_id BIGINT NOT NULL,
-    party_id VARCHAR(15) NOT NULL,
+    party_id SMALLINT NOT NULL,
     type VARCHAR(15) NOT NULL CHECK (type IN ('individual', 'organization')),
     PRIMARY KEY (bank_id, party_id),
     FOREIGN KEY (bank_id) REFERENCES Bank(bank_id) ON DELETE CASCADE
 );
 CREATE TABLE Individual (
     bank_id BIGINT NOT NULL,
-    party_id VARCHAR(15) NOT NULL,
+    party_id SMALLINT NOT NULL,
     name VARCHAR(100) NOT NULL,
     surname VARCHAR(100) NOT NULL, 
     middle_name VARCHAR(100) NULL,
     dob DATE NOT NULL,
     national_id VARCHAR(20) NOT NULL,
-    gender VARCHAR(5) NOT NULL CHECK(gender IN('male', 'female', 'intersex')),
+    gender VARCHAR(7) NOT NULL CHECK(gender IN('male', 'female', 'intersex')),
     SSN VARCHAR(9) NULL,
-    nationality VARCHAR(100) NOT NULL,
+    nationality VARCHAR(65) NOT NULL,
     registeration_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     occupation VARCHAR(10) NULL,
     martial_status VARCHAR(15) NOT NULL CHECK (martial_status IN ('married', 'not married', 'divorced', 'widowed', 'seperated')),
     disability BOOLEAN NOT NULL DEFAULT FALSE,
     disability_type VARCHAR(100) NULL,
     disability_disc VARCHAR(150) NULL,
-    annual_income VARCHAR(100) NOT NULL,
+    annual_income BIGINT NOT NULL,
     employment_status VARCHAR(100) NOT NULL CHECK(employment_status IN('employed', 'unemployed')),
     country_of_residence VARCHAR(15) NOT NULL,
     city_of_residence VARCHAR(15) NOT NULL,
@@ -76,11 +78,11 @@ CREATE TABLE Individual (
 
 CREATE TABLE Account(
 bank_id BIGINT NOT NULL,
-account_id VARCHAR(15) NOT NULL,
-Branch_id BIGINT NOT NULL,
+account_id BIGINT NOT NULL,
+Branch_id SMALLINT NOT NULL,
 account_number VARCHAR(34) NOT NULL,
 status VARCHAR(15) NOT NULL DEFAULT 'active' CHECK(status IN('active', 'blacklisted', 'frozen', 'closed')),
-account_type VARCHAR(10) NOT NULL CHECK(account_type IN('checking','savings','business','student')),
+account_type VARCHAR(10) NOT NULL CHECK(account_type IN('checking','savings','business','student', 'money market', )),
 opened_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 currency_code CHAR(3) NOT NULL,
 closed_at TIMESTAMP NULL,
@@ -111,5 +113,25 @@ FOREIGN KEY(bank_id,party_id) REFERENCES party(bank_id, party_id) ON DELETE CASC
 );
 
 CREATE TABLE LoanApplication(
+bank_id BIGINT NOT NULL,
+application_id VARCHAR(20) NOT NULL,
+applicant_party_id VARCHAR(15) NOT NULL,
+requested_amount DECIMAL(15,2) NOT NULL,
+loan_application_status VARCHAR(15) NOT NULL DEFAULT 'Pending' CHECK(loan_application_status IN('pending', 'approved', 'rejected')),
+application_sent_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+loan_type VARCHAR(15) NOT NULL CHECK (loan_type IN('personal loan', 'mortgage loan', 'auto loan', 'student loan', 'business loan','credit card loan', 'Term Loan', 'working capital loan', 'business_line_of_credit', 'equipment_financing_loan' ),
+requested_term_months SMALLINT NOT NULL CHECK (requested_term_months <=250),
+purpose_of_loan VARCHAR(250) NOT NULL,
+requested_interest_rate DECIMAL(5,2) NULL CHECK(requested_interest_rate !<0 && !=0),
+assigned_employee_id VARCHAR(30) NOT NULL,
+decision_at TIMESTAMP  NULL,
+rejection_reason VARCHAR(15) NULL,
+approved_amount DECIMAL(15,2) NULL,
+approved_term_month SMALLINT NULL CHECK(approved_term_month<=250),
+approved_interest_rate DECIMAL(5,2) NULL,
+PRIMARY KEY(bank_id,application_id),
+FOREIGN KEY(bank_id, applicant_party_id) REFERENCES party(bank_id, party_id)
+);
+
 
 
