219200b04fff995c8327029d9583c956e3ade357
Hadiqa <hadiqaimrannn0@gmail.com>
2026-07-27 10:50:58 +0300

    defined dictionaries for three geographies, US, UK and pakistan

--- synthetic_data/bank.py
+++ synthetic_data/bank.py
@@ -1,7 +1,11 @@
 from __future__ import annotations
+import random
+from typing import Dict, List
 from datetime import date
 from dataclasses import dataclass
 
+
+ 
 @dataclass
 class Address:
     building_no: str
@@ -12,6 +16,13 @@ class Address:
     
     def to_string(self) -> str:
         return f"Sk. {self.street_no}, bina: {self.building_no}, {self.district}/{self.city}, {self.country}"
+        
+        
+ALL_GEOGRAPHY = {
+    **TURKIYE_GEOGRAPHY,
+    **US_GEOGRAPHY,
+    **UK_GEOGRAPHY
+}
 
 
 @dataclass 
@@ -23,11 +34,18 @@ class Bank:
     bank_country_code: str
     bank_opened_at: date
     bank_status: str
-    bank_headquarters_address: Address  # This holds the Address object
+    bank_headquarters_address: Address
     license_number: str
     @property
     def bank_headquarters_city(self) -> str:
         return self.bank_headquarters_address.city
+        
+        
+def generate_bank_legal_name():
+
+
+
+
         
         
         
